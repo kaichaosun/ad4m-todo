718a119edeb61f4bd9c007bc79e9ef13916e2770
Kaicho Sun <kaichaosuna@gmail.com>
2022-05-30 14:00:51 +0800

    update default port

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const AD4M_ENDPOINT = "ws://localhost:4000/graphql";
+export const AD4M_ENDPOINT = "ws://localhost:12000/graphql";
 
 export const ALL_TODOS = 'all';
 export const ACTIVE_TODOS = 'active';
